d746066653a63240e3054fa4b3e06998a46dc9a2
JohanP151 <jas.pavon@yavirac.edu.ec>
2023-08-24 09:30:37 -0500

    por terminar

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,26 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { UpdateCatDto } from './dto/update-cat.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Cat } from './entities/cat.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CatsService {
-  create(createCatDto: CreateCatDto) {
+
+  constructor(
+    @InjectRepository(Cat)
+    private readonly catRepository: Repository<Cat>
+  ){}
+
+  async create(createCatDto: CreateCatDto) {
     return 'This action adds a new cat';
   }
 
-  findAll() {
-    return `This action returns all cats`;
+  async findAll() {
+    return await this.catRepository.find();
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} cat`;
   }
 
-  update(id: number, updateCatDto: UpdateCatDto) {
+  async update(id: number, updateCatDto: UpdateCatDto) {
     return `This action updates a #${id} cat`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} cat`;
   }
 }
--- src/cats/entities/cat.entity.ts
+++ src/cats/entities/cat.entity.ts
@@ -1,1 +1,18 @@
-export class Cat {}
+import { Column, DeleteDateColumn, Entity } from "typeorm";
+
+@Entity()
+export class Cat {
+
+    //@PrimaryGeneratedColumn()
+    @Column({ primary: true, generated: true })
+    id: number;
+    @Column()
+    name: string;
+    @Column()
+    age: number;
+    @Column()
+    breed: string;
+    @DeleteDateColumn()
+    deletedAt: Date;
+
+}
